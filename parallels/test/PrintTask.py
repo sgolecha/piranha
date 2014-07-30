b157da2246e29d6cfe8ee4f41fe28ba5c83f64db
Santosh Golecha <santosh.golecha@gmail.com>
2014-07-30 14:22:15 -0700

    add some comments

--- parallels/test/PrintTask.py
+++ parallels/test/PrintTask.py
@@ -1,8 +1,15 @@
 import sys
 from parallels import parallels 
 
+"""
+append the location of this task to sys.path so that the executor can 
+find it
+"""
 sys.path.append("/Users/sgolecha/Projects/github/piranha/parallels/test")
 
+"""
+This is an example of a simple task
+"""
 class PrintTask(parallels.BaseTask):
     def __init__(self, **kwargs):
         parallels.BaseTask.__init__(self, **kwargs)
@@ -15,7 +22,15 @@ class PrintTask(parallels.BaseTask):
 
 
 if __name__ == "__main__":
+    #create the TaskManager
     taskManager = parallels.TaskManager("./tasks.yaml", "./results.yaml")
+    
+    #parse the task.yaml
     taskManager.parseTaskFile()
+    
+    #parse the results.yaml file (only if you dont want a task that has been
+    #successful executed to be executed again)
     taskManager.parseResultsFile()
+    
+    #submit the tasks to execute
     taskManager.submitTasks()
